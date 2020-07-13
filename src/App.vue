440c1d44fb90bf3946ca382a42635037c273dffe
dandanloveJM <gigi834921748@qq.com>
2020-07-13 18:36:11 +0800

    初始化项目，加入icon

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
   </div>
 </template>
 
+
 <style lang="scss">
+  @import "src/assets/style/reset.scss";
+  @import "src/assets/style/var.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<button class="g-button">按钮</button>
+		<svg> <use xlink:href="#i-setting"></use> </svg>
+	</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Button extends Vue {
+
+  }
+</script>
+
+<style scoped lang="scss">
+	.g-button {
+		font-size: var(--font-size);
+		height: var(--button-height);
+		padding: 0 1em;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--border-color);
+		background: var(--button-bg);
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		vertical-align: middle;
+
+		&:hover {
+			border-color: var(--border-color-hover);
+		}
+
+		&:active {
+			background-color: var(--button-active-bg);
+		}
+
+		&:focus {
+			outline: none;
+		}
+	}
+</style>
