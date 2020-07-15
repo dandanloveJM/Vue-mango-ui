4e95c88d1ec0fa80b7e29e11e3032cd2dccb46cc
dandanloveJM <gigi834921748@qq.com>
2020-07-15 23:13:02 +0800

    自动关闭功能实现

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -6,10 +6,26 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class Toast extends Vue {
+    @Prop(Boolean) autoClose = true;
+    @Prop(Number) autoCloseDelay = 5;
+
+    mounted() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    }
+
+    close() {
+      this.$el.remove(); //把元素从页面上拿下来
+      this.$destroy();
+
+    }
 
   }
 </script>
@@ -20,12 +36,19 @@
 	$toast-bg: rgba(0, 0, 0, 0.75);
 	.toast {
 		z-index: 21;
-		font-size: $font-size; height: $toast-height; line-height: 1.8;
-		position: fixed; top: 0; left: 50%; transform: translateX(-50%); display: flex;
+		font-size: $font-size;
+		height: $toast-height;
+		line-height: 1.8;
+		position: fixed;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		display: flex;
 		color: white;
 		align-items: center;
 		background: $toast-bg;
 		border-radius: 4px;
 		box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
-		padding: 0 16px;}
+		padding: 0 16px;
+	}
 </style>
